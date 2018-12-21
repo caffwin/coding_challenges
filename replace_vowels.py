54a3cf93db90d5a213c85f4c3138ad038151a629
caffwin <catheralligator@gmail.com>
2018-12-21 03:15:22 +0000

    Replace vowels challenge

---
+++ replace_vowels.py
@@ -0,0 +1,55 @@
+"""Given list of chars, return a new copy, but with vowels replaced by '*'.
+
+For example::
+
+    >>> replace_vowels(['h', 'i'])
+    ['h', '*']
+
+    >>> replace_vowels([])
+    []
+
+    >>> replace_vowels(['o', 'o', 'o'])
+    ['*', '*', '*']
+
+    >>> replace_vowels(['z', 'z', 'z'])
+    ['z', 'z', 'z']
+
+Make sure to handle uppercase::
+
+    >>> replace_vowels(["A", "b"])
+    ['*', 'b']
+
+Do not consider `y` a vowel::
+
+    >>> replace_vowels(["y", "a", "y"])
+    ['y', '*', 'y']
+
+This should return a new list, not mutate the original::
+
+    >>> a = ['h', 'i']
+    >>> out = replace_vowels(a)
+    >>> out is not a
+    True
+
+
+"""
+
+def replace_vowels(chars):
+    """Given list of chars, return a new copy, but with vowels replaced by '*'."""
+
+    vowel_list = "aeiou"
+    new_list = []
+
+    for char in chars:
+        if char in vowel_list or char in vowel_list.upper():
+            new_list.append("*")
+        else:
+            new_list.append(char)
+
+    return new_list
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YAY!\n")
