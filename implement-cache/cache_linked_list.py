9431c1eb035d0eb918e20757320725d63ccd68e0
caffwin <catheralligator@gmail.com>
2019-01-15 01:38:16 +0000

    added remove node to LL class

--- implement-cache/cache_linked_list.py
+++ implement-cache/cache_linked_list.py
@@ -89,7 +89,20 @@ class LinkedList(object):
 
     def remove_node(self, data):
         # removes node with given data
-        # 
+        
+        if self.head is not None and self.head == data:
+            self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+            return
+        current = self.head
+
 
     def find_node(self, data):
         current = self.head
+
+        while current is not None:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
